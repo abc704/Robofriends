7a49695b9cac1e4372e99d8f0a0a9c87db56aa82
shashank <56788944+abc704@users.noreply.github.com>
2020-04-20 00:20:28 +0530

    Updates

---
+++ precache-manifest.5f368120ca134a7cbad85bc1db28e1f0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53256920d4f6c7b1a74ae3f30c20b0e9",
+    "url": "/Robofriends/index.html"
+  },
+  {
+    "revision": "b47aae68a836d61fb88a",
+    "url": "/Robofriends/static/css/2.07548e1c.chunk.css"
+  },
+  {
+    "revision": "5363a655824f4b4030f8",
+    "url": "/Robofriends/static/css/main.039eb0c5.chunk.css"
+  },
+  {
+    "revision": "b47aae68a836d61fb88a",
+    "url": "/Robofriends/static/js/2.a10038f9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Robofriends/static/js/2.a10038f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5363a655824f4b4030f8",
+    "url": "/Robofriends/static/js/main.1eb66c8b.chunk.js"
+  },
+  {
+    "revision": "d53e560d43c7171e8016",
+    "url": "/Robofriends/static/js/runtime-main.e0286ea6.js"
+  }
+]);
